fa791ed711ac878b013c6bb6bc83e5da36a26ec5
Brandon Smith <brandon@imgur.com>
2016-12-13 11:48:16 -0800

    Fixes gitpage.rb syntax error
    Signed-off-by: Brandon Smith <brandon@imgur.com>

--- gitpage.rb
+++ gitpage.rb
@@ -3,7 +3,7 @@ class Gitpage < Formula
   homepage "https://github.com/smithbh/gitpage"
   url "https://github.com/smithbh/gitpage/archive/0.0.1.tar.gz"
   version "0.0.1"
-  sha256 "dd16ecbce4218a879e1891b26c189d359ed754fa90a8cf10f57dbf7fa920e3c2""
+  sha256 "dd16ecbce4218a879e1891b26c189d359ed754fa90a8cf10f57dbf7fa920e3c2"
   def install
     bin.install "gitpage"
   end
