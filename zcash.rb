2e91a9a9c7d601e6e232fbdd798624bf4ae55c3d
Brandon Smith <brandon@imgur.com>
2016-12-20 20:58:18 -0800

    Updates to v0.0.3 of keg
    Signed-off-by: Brandon Smith <brandon@imgur.com>

--- zcash.rb
+++ zcash.rb
@@ -1,19 +1,10 @@
 class Zcash < Formula
   desc "Pre-compiled zcash binaries for OSX."
   homepage "https://github.com/smithbh/zcash"
-  url "https://github.com/smithbh/zcash/archive/v0.0.2.tar.gz"
-  version "0.0.2"
-  sha256 "bf83e79d8daa37972cc40b9d25c5678024078c7863b6cb8a8206142d8492a8f5"
+  url "https://github.com/smithbh/zcash/archive/v0.0.3.tar.gz"
+  version "0.0.3"
+  sha256 "209e4dffa909929ae617ae994a4dfdfa4422ff3cd13d61ffa96fa2d3965357e0"
 
-  bottle do
-    cellar :any
-    rebuild 1
-    sha256 "0929cf4ea3938ab5754ebefefb710444952e69154ad0c46469dfadfe91c8b1ae" => :sierra
-    sha256 "005f453126be089987d48195b2772ec0e18f217d068cb44b912f99953c5ae882" => :el_capitan
-    sha256 "fae8ad0fea117aeb332ff621019a704212cc4ef0f0bdfbfbb200a99de5ff0ec0" => :yosemite
-  end
-
-  depends_on :macos => :yosemite
   depends_on "flock" => :build
 
   def install
