fbd97fa3069f964f8ab5bcdc4eba9312ecfe1b11
Brandon Smith <brandon@imgur.com>
2016-12-20 20:50:00 -0800

    Adds osx deps
    Signed-off-by: Brandon Smith <brandon@imgur.com>

--- zcash.rb
+++ zcash.rb
@@ -6,15 +6,17 @@ class Zcash < Formula
   sha256 "046df7827cf33accb5e889758cea2684da2a2946c6dd72259459cf24c44b856c"
 
   bottle do
+    cellar :any
+    rebuild 1
     sha256 "0929cf4ea3938ab5754ebefefb710444952e69154ad0c46469dfadfe91c8b1ae" => :sierra
     sha256 "005f453126be089987d48195b2772ec0e18f217d068cb44b912f99953c5ae882" => :el_capitan
     sha256 "fae8ad0fea117aeb332ff621019a704212cc4ef0f0bdfbfbb200a99de5ff0ec0" => :yosemite
   end
 
-  depends_on "flock"
+  depends_on :macos => :yosemite
+  depends_on "flock" => :build
 
   def install
-    depends_on "flock"
     bin.install "zcashd"
     bin.install "zcash-cli"
     bin.install "zcash-tx"
