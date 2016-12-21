f77cc0ef0346818d3e30ae7e06eb8d2daa69c20c
Brandon Smith <brandon@imgur.com>
2016-12-20 22:00:34 -0800

    Updates to zcash v0.0.4
    Signed-off-by: Brandon Smith <brandon@imgur.com>

--- zcash.rb
+++ zcash.rb
@@ -1,9 +1,9 @@
 class Zcash < Formula
   desc "Pre-compiled zcash binaries for OSX."
   homepage "https://github.com/smithbh/zcash"
-  url "https://github.com/smithbh/zcash/archive/v0.0.3.tar.gz"
-  version "0.0.3"
-  sha256 "209e4dffa909929ae617ae994a4dfdfa4422ff3cd13d61ffa96fa2d3965357e0"
+  url "https://github.com/smithbh/zcash/archive/v0.0.4.tar.gz"
+  version "0.0.4"
+  sha256 "e506c9cefa7bd976fe7543dfc4a30408adee2acc73a343a89f2130e9902f984f"
 
   depends_on "flock" => :build
 
