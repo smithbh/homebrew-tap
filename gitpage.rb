a531c96d07d89ccfcc950e326d335ff93c1676c6
Brandon Smith <brandon@lawnstarter.com>
2020-02-14 09:55:10 -0600

    Opens url to current branch in default browser
    Signed-off-by: Brandon Smith <brandon@lawnstarter.com>

--- gitpage.rb
+++ gitpage.rb
@@ -1,9 +1,9 @@
 class Gitpage < Formula
   desc "Open the git repository page quickly from within a project."
   homepage "https://github.com/smithbh/gitpage"
-  url "https://github.com/smithbh/gitpage/archive/0.0.1.tar.gz"
-  version "0.0.1"
-  sha256 "dd16ecbce4218a879e1891b26c189d359ed754fa90a8cf10f57dbf7fa920e3c2"
+  url "https://github.com/smithbh/gitpage/archive/0.0.2.tar.gz"
+  version "0.0.2"
+  sha256 "2c8570bd09e5d0396d6064d8493532bbc88f394b877e149fd9b25fab4d734fc8"
   def install
     bin.install "gitpage"
   end
