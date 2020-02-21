759274d120cffaeec9b6147818d2e6a2860345cb
Brandon Smith <smithbh@me.com>
2020-02-20 21:45:32 -0600

    Updates secdel hash
    Signed-off-by: Brandon Smith <smithbh@me.com>

--- secdel.rb
+++ secdel.rb
@@ -3,7 +3,7 @@ class Secdel < Formula
   homepage "https://github.com/smithbh/secdel"
   url "https://github.com/smithbh/secdel/archive/0.0.1.tar.gz"
   version "0.0.1"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "c294ba256c3137d83ffe1cdbdef5d6b5782d03eedf564a9d1ca7184afa2eeb51"
   def install
     bin.install "secdel"
   end
