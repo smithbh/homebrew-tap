57258a2cb73e1bb4c5d51c535089307a204e5f97
Brandon Smith <brandon@lawnstarter.com>
2020-02-21 18:28:22 -0600

    Adds macOS compatibility
    Signed-off-by: Brandon Smith <brandon@lawnstarter.com>

--- secdel.rb
+++ secdel.rb
@@ -1,9 +1,9 @@
 class Secdel < Formula
   desc "Securely delete files using military-grade forensic standards."
   homepage "https://github.com/smithbh/secdel"
-  url "https://github.com/smithbh/secdel/archive/0.0.3.tar.gz"
-  version "0.0.3"
-  sha256 "ed8027ce17f0e4e7996d3c20459f7263395f450b522173eda042e990ce65a61f"
+  url "https://github.com/smithbh/secdel/archive/0.0.4.tar.gz"
+  version "0.0.4"
+  sha256 "eafb95278876ffad894073d63dbb5312e52016e18de5ba9ff82c98b0c0f4333a"
   depends_on "khell/srm/srm"
 
   def install
