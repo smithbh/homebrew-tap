3eeed9bf341d9d6263d45c5f6a85e47843ac209c
Brandon Smith <smithbh@me.com>
2025-12-18 02:22:32 -0600

    Updates digtor
    Signed-off-by: Brandon Smith <smithbh@me.com>

--- digtor.rb
+++ digtor.rb
@@ -1,12 +1,11 @@
 class Digtor < Formula
   desc "Check if an ipv4 address is associated with a Tor Exit Node."
   homepage "https://github.com/smithbh/digtor"
-  url "https://github.com/smithbh/digtor/archive/0.0.4.tar.gz"
-  version "0.0.4"
+  url "https://github.com/smithbh/digtor/archive/1.0.0.tar.gz"
+  version "1.0.0"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
   def install
     bin.install "digtor"
   end
 end
-
