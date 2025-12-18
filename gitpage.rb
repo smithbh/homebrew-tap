9dd20bd0c98fc3069d265ed3d68ee28a07f3ecd1
Brandon Smith <smithbh@me.com>
2025-12-18 15:01:10 -0600

    Updates gitpage
    Signed-off-by: Brandon Smith <smithbh@me.com>

--- gitpage.rb
+++ gitpage.rb
@@ -1,9 +1,9 @@
 class Gitpage < Formula
   desc "Open the git repository page quickly from within a project."
   homepage "https://github.com/smithbh/gitpage"
-  url "https://github.com/smithbh/gitpage/archive/0.0.3.tar.gz"
-  version "0.0.3"
-  sha256 "76ced82d910f786e5c5bfd7facdca451f1aeb0fec12ef95d4f2f4d118c3aea29"
+  url "https://github.com/smithbh/gitpage/archive/0.0.4.tar.gz"
+  version "0.0.4"
+  sha256 "9e5b0c0255a78c3edf589fd56de57e8f357e807b9396d1fcb6f65ecf847a7db4"
   def install
     bin.install "gitpage"
   end
