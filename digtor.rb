27f360fd9b846eaa6fb601f7b4039d405c259e20
Brandon Smith <smithbh@me.com>
2025-12-18 01:16:52 -0600

    Fixes hash syntax
    Signed-off-by: Brandon Smith <smithbh@me.com>

--- digtor.rb
+++ digtor.rb
@@ -3,7 +3,7 @@ class Digtor < Formula
   homepage "https://github.com/smithbh/digtor"
   url "https://github.com/smithbh/digtor/archive/0.0.4.tar.gz"
   version "0.0.4"
-  sha256 "sha256:341a54463b8727568e1a84e8066f0183f01b5d349a6a548c718581d072c0bd8c"
+  sha256 "341a54463b8727568e1a84e8066f0183f01b5d349a6a548c718581d072c0bd8c"
 
   def install
     bin.install "digtor"
