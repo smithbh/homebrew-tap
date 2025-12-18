b496b96cb283b9dd3853d771de9cd58c1f55e545
Brandon Smith <smithbh@me.com>
2025-12-18 02:11:06 -0600

    Updates digtor
    Signed-off-by: Brandon Smith <smithbh@me.com>

--- digtor.rb
+++ digtor.rb
@@ -3,7 +3,7 @@ class Digtor < Formula
   homepage "https://github.com/smithbh/digtor"
   url "https://github.com/smithbh/digtor/archive/0.0.4.tar.gz"
   version "0.0.4"
-  sha256 "0cf860c3636f4a506584253d001eeb0d50614354bd4529d3517a04cd36b0bca1"
+  sha256 "a1598f2b65d1ba2f1ed9300800955ca1a967d683e323628f1d8f8e26536ca977"
 
   def install
     bin.install "digtor"
