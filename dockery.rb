327f7149eb0f9ee0919e1f9681606f3a8be100fb
Brandon Smith <smithbh@me.com>
2025-12-18 17:11:47 -0600

    Fixes dockery release binary url
    Signed-off-by: Brandon Smith <smithbh@me.com>

--- dockery.rb
+++ dockery.rb
@@ -1,7 +1,7 @@
 class Dockery < Formula
   desc "Helper functions for working with docker on OSX."
   homepage "https://github.com/smithbh/dockery"
-  url "https://github.com/smithbh/dockery/archive/v0.1.0.tar.gz"
+  url "https://github.com/smithbh/dockery/archive/0.1.0.tar.gz"
   version "0.1.0"
   sha256 "84d1e3ac8886ef1e9bf6eac5fa42ab463092fb7955cd8567e3cdf6b00b11e077"
   def install
