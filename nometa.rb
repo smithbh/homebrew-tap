4ceb8f08f434957700ba48ded7f1271368e77ec4
Brandon Smith <smithbh@me.com>
2025-12-18 19:56:04 -0600

    Fixes nometa binary url
    Signed-off-by: Brandon Smith <smithbh@me.com>

--- nometa.rb
+++ nometa.rb
@@ -1,7 +1,7 @@
 class Secdel < Formula
   desc "Perform a deep sanitization of file metadata."
   homepage "https://github.com/smithbh/nometa"
-  url "https://github.com/smithbh/secdel/archive/0.0.1.tar.gz"
+  url "https://github.com/smithbh/nometa/archive/0.0.1.tar.gz"
   version "0.0.1"
   sha256 "f853938515bc81dafe6070cde419bb4f80cf903795f3812eb8c4a0089d39f868"
   depends_on "exiftool"
